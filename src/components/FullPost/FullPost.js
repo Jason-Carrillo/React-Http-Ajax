76597d6ecfb57638fb2711e737551dadc72d1175
Jason Carrillo <jason.carrillo182@gmail.com>
2021-02-09 14:33:06 -0700

    added axios import to individual post

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
 
 import './FullPost.css';
+import axios from "axios";
 
 class FullPost extends Component {
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if (this.props.id) {
+            axios.get('http://jsonplaceholder.typicode.com/posts/' + this.props.id)
+                .then(response => {
+                    console.log(response)
+                })
+        }
+    }
     render () {
         let post = <p style={{textAlign: 'center'}}> Please select a Post! </p>;
             if (this.props.id) {
