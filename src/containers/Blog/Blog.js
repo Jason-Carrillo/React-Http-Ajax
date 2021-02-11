2588aa3643279e931db699f75ab2dce661d03eef
Jason Carrillo <jason.carrillo182@gmail.com>
2021-02-11 12:41:22 -0700

    imported NEW POST

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -3,6 +3,7 @@ import { Route } from 'react-router-dom'
 
 import './Blog.css';
 import Posts from './Posts/Posts'
+import NewPost from "./NewPost/NewPost";
 
 class Blog extends Component {
     state = {
@@ -33,6 +34,7 @@ class Blog extends Component {
 
                 {/*Displaying components can be passed through this for specific pages*/}
                 <Route path="/" exact component={Posts} />
+                <Route path="/new-posts" component={NewPost} />
 
             </div>
         );
