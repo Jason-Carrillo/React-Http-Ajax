ce57bfc40284e7c75f54a882959ff9326721aa6b
Jason Carrillo <jason.carrillo182@gmail.com>
2021-02-12 08:42:52 -0700

    imported full post to Blog

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -4,6 +4,7 @@ import { Route, NavLink } from 'react-router-dom'
 import './Blog.css';
 import Posts from './Posts/Posts'
 import NewPost from "./NewPost/NewPost";
+import FullPost from "./FullPost/FullPost";
 
 class Blog extends Component {
     state = {
